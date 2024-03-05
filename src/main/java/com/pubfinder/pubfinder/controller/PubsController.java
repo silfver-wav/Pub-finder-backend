0d24e930a812abe2836938a659fd2188e860abb2
Linus Silfver <linus.silfver@gmail.com>
2024-03-05 16:31:41 +0100

    Added PubsController and unit tests for PubsController

---
+++ src/main/java/com/pubfinder/pubfinder/controller/PubsController.java
@@ -0,0 +1,42 @@
+package com.pubfinder.pubfinder.controller;
+
+import com.pubfinder.pubfinder.dto.PubDTO;
+import com.pubfinder.pubfinder.mapper.PubMapper;
+import com.pubfinder.pubfinder.service.PubsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class PubsController {
+
+    @Autowired
+    private PubsService pubsService;
+
+    @GetMapping("/getPubs/{lat}/{lng}/{radius}")
+    public ResponseEntity<List<PubDTO>> getPubs(@PathVariable("lat") Double lat, @PathVariable("lng") Double lng, @PathVariable("radius") Double radius) {
+        return pubsService.getPubs(lat, lng, radius);
+    }
+
+    @GetMapping("/getPub/{name}")
+    public ResponseEntity<PubDTO> getPub(@PathVariable("name") String name) {
+        return pubsService.getPubByName(name);
+    }
+
+    @PostMapping("/savePub")
+    public ResponseEntity<PubDTO> savePub(@RequestBody PubDTO pub) {
+        return pubsService.savePub(PubMapper.INSTANCE.dtoToEntity(pub));
+    }
+
+    @PutMapping("/editPub")
+    public ResponseEntity<PubDTO> editPub(@RequestBody PubDTO pub) {
+        return pubsService.editPub(PubMapper.INSTANCE.dtoToEntity(pub));
+    }
+
+    @DeleteMapping("/deletePub")
+    public ResponseEntity<PubDTO> deletePub(@RequestBody PubDTO pub) {
+        return pubsService.deletePub(PubMapper.INSTANCE.dtoToEntity(pub));
+    }
+
+}
---
+++ src/test/java/com/pubfinder/pubfinder/controller/PubsControllerTest.java
@@ -0,0 +1,98 @@
+package com.pubfinder.pubfinder.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pubfinder.pubfinder.dto.PubDTO;
+import com.pubfinder.pubfinder.mapper.PubMapper;
+import com.pubfinder.pubfinder.service.PubsService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PubsController.class)
+public class PubsControllerTest {
+
+    @MockBean
+    private PubsService pubsService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void getPubsTest() throws Exception {
+        List<PubDTO> pubs = new ArrayList<>(List.of(pub));
+        when(pubsService.getPubs(1.0,1.0,1.0)).thenReturn(ResponseEntity.ok().body(pubs));
+
+        mockMvc.perform(get("/getPubs/{lat}/{lng}/{radius}", 1.0, 1.0, 1.0)).andExpect(status().isOk()); //.andExpect(content().json(new ObjectMapper().writeValueAsString(pubs)));
+    }
+
+    @Test
+    public void getPubByNameTest() throws Exception {
+        when(pubsService.getPubByName("name")).thenReturn(ResponseEntity.ok().body(pub));
+
+        mockMvc.perform(get("/getPub/{name}", "name")).andExpect(status().isOk()); //.andExpect(content().json(new ObjectMapper().writeValueAsString(pubs)));
+    }
+
+    @Test
+    public void savePubTest() throws Exception {
+        when(pubsService.savePub(PubMapper.INSTANCE.dtoToEntity(pub))).thenReturn(ResponseEntity.status(HttpStatus.CREATED).body(pub));
+
+        mockMvc.perform(post("/savePub").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(pub)))
+                .andExpect(status().isCreated()).andDo(print());
+    }
+
+    @Test
+    public void savePubTest_BAD_REQUEST() throws Exception {
+        when(pubsService.savePub(null)).thenReturn(ResponseEntity.badRequest().build());
+
+        mockMvc.perform(post("/savePub").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest()).andDo(print());
+    }
+
+    @Test
+    public void editPubTest() throws Exception {
+        when(pubsService.editPub(PubMapper.INSTANCE.dtoToEntity(pub))).thenReturn(ResponseEntity.ok().body(pub));
+
+        mockMvc.perform(put("/editPub").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(pub))).andExpect(status().isOk());
+    }
+
+    @Test
+    public void editPubTest_BAD_REQUEST() throws Exception {
+        when(pubsService.editPub(PubMapper.INSTANCE.dtoToEntity(pub))).thenReturn(ResponseEntity.badRequest().build());
+
+        mockMvc.perform(put("/editPub").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(pub))).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void editPubTest_NOT_FOUND() throws Exception {
+        when(pubsService.editPub(PubMapper.INSTANCE.dtoToEntity(pub))).thenReturn(ResponseEntity.notFound().build());
+
+        mockMvc.perform(put("/editPub").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(pub))).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deletePubTest() throws Exception {
+        when(pubsService.deletePub(PubMapper.INSTANCE.dtoToEntity(pub))).thenReturn(ResponseEntity.ok().build());
+
+        mockMvc.perform(delete("/deletePub").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(pub))).andExpect(status().isOk());
+    }
+
+    PubDTO pub = new PubDTO(UUID.randomUUID(), "name", 1.0, 1.0,"open", "location", "desc");
+}
